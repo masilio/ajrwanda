84df42d1b5f183c695e01a9e7a831c55cc44f7cf
masilio <felinshimiyimana@gmail.com>
2022-12-23 11:39:20 +0200

    changes made

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className="container-fluid ajr__navbar">
       <div className="ajr__navbar-links">
         <div className="ajr__navbar-links_logo">
-          <img src={logo} />
+          <img src={logo}  alt=""/>
           <span className="header__text">ART FOR JUSTICE RWANDA </span>
         </div>
         
